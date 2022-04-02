7b378d5447b730117e72aaf5a9f491961a873957
Arya Gupta <arya.gupta99999@gmail.com>
2022-04-02 22:00:48 +0530

    Footer file
    Footer file contains name of forum along with credits

---
+++ partials/_footer.php
@@ -0,0 +1,8 @@
+<footer class="my-5 pt-5 text-muted text-center text-small" style="bottom: 0; width: 100%;">
+    <p class="mb-1">&copy; 2022 College Forum</p>
+    <ul class="list-inline">
+        <li class="list-inline-item"><a target="_blank" href="https://www.linkedin.com/in/arya-gupta2002/">Created by - Arya Gupta</a></li>
+    </ul>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
